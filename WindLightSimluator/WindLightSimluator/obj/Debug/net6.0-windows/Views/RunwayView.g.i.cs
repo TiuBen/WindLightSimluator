444f3861aa3cb9950960474ec8a2a5f05d38b71f
TiuBen <shentiuben@gmail.com>
2026-03-24 14:51:03 +0800

    1212414

--- WindLightSimluator/WindLightSimluator/obj/Debug/net6.0-windows/Views/RunwayView.g.i.cs
+++ WindLightSimluator/WindLightSimluator/obj/Debug/net6.0-windows/Views/RunwayView.g.i.cs
@@ -81,7 +81,7 @@ namespace WindLightSimluator.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WindLightSimluator;V1.0.0.0;component/views/runwayview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WindLightSimluator;component/views/runwayview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\RunwayView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
