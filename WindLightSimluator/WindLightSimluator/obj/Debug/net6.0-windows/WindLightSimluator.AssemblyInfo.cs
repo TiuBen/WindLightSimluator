3782f3a8d689dc2519d55d627757f9e8b10f6d36
TiuBen <shentiuben@gmail.com>
2026-05-14 21:21:24 +0800

    123321

--- WindLightSimluator/WindLightSimluator/obj/Debug/net6.0-windows/WindLightSimluator.AssemblyInfo.cs
+++ WindLightSimluator/WindLightSimluator/obj/Debug/net6.0-windows/WindLightSimluator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindLightSimluator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3e15f9ab0a6717c9633fcabb797110e3ecc1c37")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindLightSimluator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindLightSimluator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
