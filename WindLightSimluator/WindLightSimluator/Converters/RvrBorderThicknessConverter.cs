38ff8446d2fd62edc113358a7018fdda5adc32d8
TiuBen <shentiuben@gmail.com>
2026-05-15 00:29:48 +0800

    312431

---
+++ WindLightSimluator/WindLightSimluator/Converters/RvrBorderThicknessConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows;
+
+namespace WindLightSimluator.Converters
+{
+    public class RvrBorderThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return 0;
+
+            if (!int.TryParse(value.ToString(), out int v))
+                return 0 ;
+            if (v <= 2000)
+                return 4;
+
+            return 0;
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
